9471c1580823130040173269c38f61ba043f28b8
Lowliet <lowdata114@gmail.com>
2024-03-13 23:27:05 -0700

    Cutscene + Quit Game

---
+++ Heckpooper/scripts/FinalCutsceneTrigger.gd
@@ -0,0 +1,25 @@
+extends Area3D
+
+@onready var cutscene_player = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+
+
+
+func _on_area_entered(area):
+	#print(area.get_parent().get_name())
+	if(area.is_in_group("Player")):
+		#print("Cutscene trigger")
+		cutscene_player.play("Final_Cutscene")
+		await get_tree().create_timer(4.0).timeout
+		get_tree().quit()
